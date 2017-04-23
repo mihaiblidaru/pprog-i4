e46ae55953110a83509102885a53a770784b6ad5
mihai <mihai@proyecto>
2017-04-23 10:58:50 +0000

    Añadidas enumeraciones para el control de errores de las funciones load y save

--- include/game_management.h
+++ include/game_management.h
@@ -5,5 +5,13 @@ STATUS game_management_save(Game* game, char* filename);
 STATUS game_management_load(Game* game, char* filename);
 STATUS game_management_start_from_file(Game* game, char* spacesFile, char* objectsFile, char* linksfile, char* playersFile);
 
+typedef enum {
+    PROTECTED_FILE, SAVE_OK, WRITE_FAILED
+} SAVE_STATUS;
+
+typedef enum {
+    LOAD_OK, IO_ERROR, NO_PLAYER, NO_SPACES, NO_LINKS
+} LOAD_STATUS;
+
 
 #endif

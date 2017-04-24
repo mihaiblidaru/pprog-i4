afd88b7babbc3e38b85b843eb5514ec898e85727
mihai <mihai@proyecto>
2017-04-24 23:39:16 +0000

    Añadida documentación de los tests

--- src/command_test.c
+++ src/command_test.c
@@ -71,7 +71,11 @@ int main(int argc, char** argv) {
  * @endcond
  */
 
-
+/*
+ * @test Prueba la función de creación de un gestor de comandos
+ * @pre Condiciones normales para la prueba
+ * @post Un puntero no nulo al al gestor de comandos creado
+ */
 void test1_command_ini(){
     Command *cmd = NULL;
     cmd = Command_ini();
@@ -79,17 +83,33 @@ void test1_command_ini(){
     Command_destroy(cmd);
 }
 
+/*
+ * @test Pruba la función que libera memoria usada por un gestor de comandos
+ * @pre El gestor de comandos ha sido previamente inicializado
+ * @post La salida esperada es OK
+ */
+
 void test1_command_destroy(){
     Command* cmd = NULL;
     cmd = Command_ini();
     PRINT_TEST_RESULT(Command_destroy(cmd) == OK);
 }
 
+/*
+ * @test Pruba la función que libera memoria usada por un gestor de comandos
+ * @pre El gestor de comandos no ha sido inicializado
+ * @post La salida esperada es ERROR
+ */
 void test2_command_destroy(){
     Command* cmd = NULL;
     PRINT_TEST_RESULT(Command_destroy(cmd) == ERROR);
 }
 
+/*
+ * @test Prueba la función que guarda el tipo de comando en un gestor de comandos
+ * @pre El gestor de comandos ha sido inicializado previamente
+ * @post La salida esperada es OK
+ */
 void test1_command_set_cmd(){
     Command* cmd = NULL;
     cmd = Command_ini();
@@ -97,6 +117,11 @@ void test1_command_set_cmd(){
     Command_destroy(cmd);
 }
 
+/*
+ * @test Prueba la función que guarda el tipo de comando en un gestor de comandos
+ * @pre El gestor de comandos ha sido inicializado previamente pero el comando no es valido
+ * @post La salida esperada es ERROR
+ */
 void test2_command_set_cmd(){
     Command* cmd = NULL;
     cmd = Command_ini();
@@ -104,11 +129,21 @@ void test2_command_set_cmd(){
     Command_destroy(cmd);
 }
 
+/*
+ * @test Prueba la función que guarda el tipo de comando en un gestor de comandos
+ * @pre El gestor de comandos no ha sido inicializado previamente
+ * @post La salida esperada es ERROR
+ */
 void test3_command_set_cmd(){
     Command* cmd = NULL;
     PRINT_TEST_RESULT(Command_set_cmd(cmd, LEAVE) == ERROR);
 }
 
+/*
+ * @test Prueba la función que guarda un argumento de un comando en un gestor de comandos
+ * @pre El gestor de comandos ha sido inicializado previamente
+ * @post La salida esperada es OK
+ */
 void test1_command_set_cmd_arg(){
     Command* cmd = NULL;
     cmd = Command_ini();
@@ -116,6 +151,11 @@ void test1_command_set_cmd_arg(){
     Command_destroy(cmd);
 }
 
+/*
+ * @test Prueba la función que guarda un argumento de un comando en un gestor de comandos
+ * @pre La posición en la que se quiere guardar es invalida
+ * @post La salida esperada es ERROR
+ */
 void test2_command_set_cmd_arg(){
     Command* cmd = NULL;
     cmd = Command_ini();
@@ -123,11 +163,21 @@ void test2_command_set_cmd_arg(){
     Command_destroy(cmd);
 }
 
+/*
+ * @test Prueba la función que guarda un argumento de un comando en un gestor de comandos
+ * @pre El gestor de comandos no ha sido inicializado previamente
+ * @post La salida esperada es ERROR
+ */
 void test3_command_set_cmd_arg(){
     Command* cmd = NULL;
     PRINT_TEST_RESULT(Command_set_cmd_arg(cmd,"test", 1) == ERROR);
 }
 
+/*
+ * @test Prueba la función que guarda un argumento de un comando en un gestor de comandos
+ * @pre El argumento es un puntero a NULL
+ * @post La salida esperada es ERROR
+ */
 void test4_command_set_cmd_arg(){
     Command* cmd = NULL;
     cmd = Command_ini();
@@ -135,6 +185,11 @@ void test4_command_set_cmd_arg(){
     Command_destroy(cmd);
 }
 
+/*
+ * @test Prueba la función que devuelve el tipo de comando guardado en un gestor de comandos
+ * @pre El gestor de comandos ha sido inicializado previamente y se le ha asignado el comando GO
+ * @post La salida esperada es GO, el comando previamente asignado
+ */
 void test1_command_get_cmd(){
     Command* cmd = NULL;
     cmd = Command_ini();
@@ -143,11 +198,21 @@ void test1_command_get_cmd(){
     Command_destroy(cmd);
 }
 
+/*
+ * @test Prueba la función que devuelve el tipo de comando guardado en un gestor de comandos
+ * @pre El gestor de comandos no ha sido inicializado previamente
+ * @post La salida esperada es UNKNOWN, comando desconocido
+ */
 void test2_command_get_cmd(){
     Command* cmd = NULL;
     PRINT_TEST_RESULT(Command_get_cmd(cmd) == UNKNOWN);
 }
 
+/*
+ * @test Prueba la función que devuelve el tipo de comando guardado en un gestor de comandos
+ * @pre El gestor de comandos ha sido inicializado previamente pero no se le ha asignado un comando
+ * @post La salida esperada es NO_CMD
+ */
 void test3_command_get_cmd(){
     Command* cmd = NULL;
     cmd = Command_ini();
@@ -155,19 +220,33 @@ void test3_command_get_cmd(){
     Command_destroy(cmd);
 }
 
+/*
+ * @test Prueba la función que devuelve un argumento de un comando guardado en un gestor de comandos
+ * @pre El gestor de comandos ha sido inicializado previamente y se le ha asignado un argumento "test"
+ * @post La salida esperada es "test", el argumento añadido previamente
+ */
 void test1_command_get_cmd_arg(){
    Command* cmd = NULL;
    cmd = Command_ini();
-   Command_set_cmd_arg(cmd,"test",1);
+   Command_set_cmd_arg(cmd, "test", 1);
    PRINT_TEST_RESULT(strcmp(Command_get_cmd_arg(cmd,1), "test") == 0);
    Command_destroy(cmd);
 }
 
+/*
+ * @test Prueba la función que devuelve un argumento de un comando guardado en un gestor de comandos
+ * @pre El gestor de comandos no ha sido inicializado previamente
+ * @post La salida esperada es NULL
+ */
 void test2_command_get_cmd_arg(){
    Command* cmd = NULL;
    PRINT_TEST_RESULT(Command_get_cmd_arg(cmd,1) == NULL);
 }
-
+/*
+ * @test Prueba la función que devuelve un argumento de un comando guardado en un gestor de comandos
+ * @pre El gestor de comandos ha sido inicializado previamente pero no se le ha asignado un argumento
+ * @post La salida esperada es una cadena vacia
+ */
 void test3_command_get_cmd_arg(){
    Command* cmd = NULL;
    cmd = Command_ini();
@@ -175,6 +254,11 @@ void test3_command_get_cmd_arg(){
    Command_destroy(cmd);
 }
 
+/*
+ * @test Prueba la función que limpia los campos de un commando
+ * @pre El gestor de comandos ha sido inicializado previamente y se ha guardado un comando dentro
+ * @post La salida esperada es NO_CMD y una cadena vacia
+ */
 void test1_command_clear(){
    Command* cmd = NULL;
    cmd = Command_ini();
@@ -185,6 +269,11 @@ void test1_command_clear(){
    Command_destroy(cmd);
 }
 
+/*
+ * @test Prueba la función que limpia los campos de un commando
+ * @pre El gestor de comandos no ha sido inicializado previamente
+ * @post La salida esperada es ERROR
+ */
 void test2_command_clear(){
    Command* cmd = NULL;
    PRINT_TEST_RESULT(Command_clear(cmd) == ERROR);
